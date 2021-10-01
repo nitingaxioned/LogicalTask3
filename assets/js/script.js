98624a34a10c6e156665d5ada99764ba7a1a9b6b
Nitin <niting@axioned.com>
2021-10-01 13:07:37 +0530

    js logic fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 /* Author: 
 
 */
-var n, chars="";
+var n;
 var array = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" ] , ansArray=[];
+let str;
 
 function validate(){
     n = parseInt( document.getElementById("numb").value );
-    if(n>=0)
-        strArrayOfnum(n.toString(), 0, "");
+    if(n>=0){
+        strArrayOfnum(n.toString(), 0, "" );
+        document.getElementById("output-txt").innerHTML ="Output:- <br><br>"+ansArray;
+        ansArray=[];
+        console.log(ansArray);
+    }
     else{
         document.getElementById("error").innerHTML = "Error : "+document.getElementById("numb").value+" is not a Natural Number";
     }
@@ -19,34 +24,17 @@ function reclear(){
 }
 
 function strArrayOfnum(str, ptr, tmpstr){
-    console.log("inn the fun");
-    console.log(str);
-    console.log(typeof(str));
-    console.log(str.lenght);
-    console.log(str.toString().lenght);
-    if( ptr == str.lenght - 1 )
+    if( ptr == str.length )
         ansArray.push( tmpstr );
     else{
-        chars = array[str.charAt(ptr)];
-        console.log(chars +" "+ptr);
-        console.log(ansArray);
+        let chars = array[str.charAt(ptr)];
         for(let i=0; i <(chars.length) ; i++){
-            console.log("inn the loop");
             tmpstr = tmpstr.concat(chars.charAt(i));
             strArrayOfnum(str, ptr+1, tmpstr);
-            tmpstr.splice(-1,1); 
+            tmpstr = tmpstr.slice(0,-1);
         }
-        // for(let i of chars){
-        //     tmpstr.concat(chars.charAt(i));
-        //     strArrayOfnum(str, i+1, tmpstr);
-        //     tmpstr.splice(-1,1);
-        // }
     }
-    
 }
-
-
-
 
 
 
